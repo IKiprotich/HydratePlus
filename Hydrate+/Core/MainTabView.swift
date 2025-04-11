5a1d05076b7c75e4e9c97dc20ae9ad2d6037baf1
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-11 12:49:15 +0300

    added a tab view

---
+++ Hydrate+/Core/MainTabView.swift
@@ -0,0 +1,54 @@
+//
+//  MainTabView.swift
+//  Hydrate+
+//
+//  Created by Ian   on 11/04/2025.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    @State private var selectedTab = 0
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "drop.fill")
+                }
+                .tag(0)
+            
+            StatisticsView()
+                .tabItem {
+                    Label("Statistics", systemImage: "chart.bar.fill")
+                }
+                .tag(1)
+            
+            ReminderView()
+                .tabItem {
+                    Label("Reminders", systemImage: "bell.fill")
+                }
+                .tag(2)
+            
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person.fill")
+                }
+                .tag(3)
+        }
+        .tint(Color.waterBlue)
+        .onAppear {
+           
+            let appearance = UITabBarAppearance()
+            appearance.backgroundColor = UIColor.white
+            appearance.shadowColor = UIColor(Color.waterBlue.opacity(0.2))
+            
+            UITabBar.appearance().standardAppearance = appearance
+            UITabBar.appearance().scrollEdgeAppearance = appearance
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
