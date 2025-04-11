d047b65faabfab1136ea9be19051217c26d1828d
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-11 12:51:36 +0300

    added the contentView file

---
+++ Hydrate+/Core/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Hydrate+
+//
+//  Created by Ian   on 11/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @EnvironmentObject var viewModel: AuthViewModel
+    
+    var body: some View {
+        Group {
+            if viewModel.userSession != nil {
+                MainTabView()
+            } else {
+                LoginView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(AuthViewModel())
+    }
+}
